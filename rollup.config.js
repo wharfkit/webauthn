f88295dbda0831b11fc7d4cd55552b02463cc88f
dafuga <danielfugere28@gmail.com>
2026-03-16 23:39:54 -0700

    fix: bundle elliptic in library builds

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import dts from 'rollup-plugin-dts'
 import typescript from '@rollup/plugin-typescript'
 
 import pkg from './package.json'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import {nodeResolve} from '@rollup/plugin-node-resolve'
 
 const name = pkg.name
 const license = fs.readFileSync('LICENSE').toString('utf-8').trim()
@@ -16,9 +19,7 @@ const banner = `
  */
 `.trim()
 
-const external = Object.keys(pkg.dependencies)
-
-/** @type {import('rollup').RollupOptions} */
+const external = Object.keys(pkg.dependencies).filter((dependency) => dependency !== 'elliptic')
 export default [
     {
         input: 'src/index.ts',
@@ -28,7 +29,7 @@ export default [
             format: 'cjs',
             sourcemap: true,
         },
-        plugins: [typescript({target: 'es6'})],
+        plugins: [nodeResolve(), json(), commonjs(), typescript({target: 'es6'})],
         external,
     },
     {
@@ -39,7 +40,7 @@ export default [
             format: 'esm',
             sourcemap: true,
         },
-        plugins: [typescript({target: 'es2020'})],
+        plugins: [nodeResolve(), json(), commonjs(), typescript({target: 'es2020'})],
         external,
     },
     {
